21e9ef4e306eeb8136b5feebc4f529b24a7e636b
Greg280269 <113647750+Greg280269@users.noreply.github.com>
2023-10-31 00:01:41 -0500

    Update Tipo_ComprobanteController.java

--- src/main/java/pe/edu/upc/aaw/proyecto_awb/controllers/Tipo_ComprobanteController.java
+++ src/main/java/pe/edu/upc/aaw/proyecto_awb/controllers/Tipo_ComprobanteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/tipo comprobante")
+@RequestMapping("/tipo_comprobante")
 public class Tipo_ComprobanteController {
     @Autowired
     private ITipoComprobanteService tdS;
